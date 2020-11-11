4c07523eed0969f90ca0f4369a7bbdf103206d35
inazarm <inazar.m@gmail.com>
2020-11-11 16:00:10 +0500

    bubble sorting completed

---
+++ BubbleSortingApplication/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSortingApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 45, 12, 14, 15, 32, 15, 11, 1, 8, 9 };
+            int temp;
+            Console.WriteLine("before sorting");
+            foreach (int p in arr)
+            {
+                Console.WriteLine(p + " ");
+            }
+            for (int j = 0; j <= arr.Length-2; j++)
+            {
+                for (int i = 0; i <= arr.Length-2; i++)
+                {
+                    if (arr[i]>arr[i+1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("after sorting");
+            foreach (int p in arr)
+            {
+                Console.WriteLine(p+ " ");
+            }
+        }
+    }
+}
